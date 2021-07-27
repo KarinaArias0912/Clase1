ce34cb7dd1b5c277c97754c6cfaee1d6e13bbd2b
KariArias <karinaariasandrea@gmail.com>
2021-07-26 21:57:48 -0300

    NavBar

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar.js";
+
+const Header = () => 
+
+<header>
+    <h1>Zero Waste Store</h1>
+    <NavBar/>
+</header>
+
+export default Header 
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () =>
+
+<nav>
+    <a href= "#">Cocina</a>
+    
+    <a href= "#">Cuidado Personal</a>
+
+    <a href= "#">Limpieza</a>
+    
+    <a href= "#">Accesorios</a>
+</nav>
+
+export default NavBar 
